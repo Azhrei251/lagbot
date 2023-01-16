d5e9a5419956951342af359f4a0590fec96a41e1
Karl Beier <karl.beier1@gmail.com>
2023-01-16 12:27:26 +1100

    Fix spotify play not working when bot is not in voice

--- src/main/kotlin/com/azhapps/lagbot/commands/BasePlayCommand.kt
+++ src/main/kotlin/com/azhapps/lagbot/commands/BasePlayCommand.kt
@@ -16,20 +16,11 @@ abstract class BasePlayCommand(messageEvent: MessageCreateEvent) : BaseCommand(m
         if (requesterInVoice) {
             val songRequest = event.messageContent.substringAfter(' ').substringBefore("?playlist")
             if (botInVoice) {
-                if (songRequest.contains("open.spotify.com")) {
-                    Main.scope.launch {
-                        SpotifyRepository.getSearchTerms(songRequest).forEach {
-                            AudioUtil.playSong(event.server.get(), it, event.channel, playTime)
-                        }
-                    }
-                } else {
-                    AudioUtil.playSong(event.server.get(), songRequest, event.channel, playTime)
-                }
-
+                playOrLookupSong(songRequest)
             } else {
                 event.messageAuthor.connectedVoiceChannel.get().connect().thenAccept {
                     AudioUtil.connect(event.server.get(), it)
-                    AudioUtil.playSong(event.server.get(), songRequest, event.channel, playTime)
+                    playOrLookupSong(songRequest)
                 }.whenComplete { _, t ->
                     t.printStackTrace()
                     event.channel.sendMessage("Something went wrong:\n${t.message}")
@@ -38,6 +29,18 @@ abstract class BasePlayCommand(messageEvent: MessageCreateEvent) : BaseCommand(m
 
         } else {
             event.channel.sendMessage("Join a voice channel first!")
+        }
+    }
+
+    private fun playOrLookupSong(songRequest: String) {
+        if (songRequest.contains("open.spotify.com")) {
+            Main.scope.launch {
+                SpotifyRepository.getSearchTerms(songRequest).forEach {
+                    AudioUtil.playSong(event.server.get(), it, event.channel, playTime)
+                }
+            }
+        } else {
+            AudioUtil.playSong(event.server.get(), songRequest, event.channel, playTime)
         }
     }
 }
