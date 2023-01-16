05e24b979112b0e81289ff6da1039fd967598972
Karl Beier <karl.beier1@gmail.com>
2023-01-16 12:17:37 +1100

    Remove unused code

--- src/main/kotlin/com/azhapps/lagbot/audio/AudioUtil.kt
+++ src/main/kotlin/com/azhapps/lagbot/audio/AudioUtil.kt
@@ -35,15 +35,6 @@ object AudioUtil {
             registerSourceManager(SoundCloudAudioSourceManager.Builder().build())
         }
     }
-    private val EMAIL_ADDRESS: Pattern = Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-                "\\@" +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-                "(" +
-                "\\." +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-                ")+"
-    )
 
     fun connect(server: Server, audioConnection: AudioConnection) {
         val player = playerMap[server.id] ?: playerManager.createPlayer().apply {
