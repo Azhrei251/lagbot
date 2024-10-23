362a213553f23ad6fae56c5408de51350ab0671f
Karl Beier <karl.beier1@gmail.com>
2024-10-23 21:13:06 +1100

    Clean up

--- src/main/kotlin/com/azhapps/lagbot/local/LocalTrackChecker.kt
+++ src/main/kotlin/com/azhapps/lagbot/local/LocalTrackChecker.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import org.apache.commons.text.similarity.FuzzyScore
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.util.*
@@ -20,20 +21,21 @@ private const val MIN_TOTAL_LENGTH_TO_CONSIDER = 4
 
 class LocalTrackChecker(
     private val ioScope: CoroutineScope,
+    private val audioDir: File = File(PropertiesUtil.get(PropertiesUtil.LOCAL_AUDIO_DIR)),
+    private val scorer: FuzzyScore = FuzzyScore(Locale.getDefault()),
+    private val logger: Logger = LoggerFactory.getLogger(LocalTrackChecker::class.java),
 ) {
-
     // TODO Some sort of save to disk + periodic update (or folder watching)
     private val localTracks: MutableList<LocalAudioTrackInfo> = mutableListOf()
-    private val scorer: FuzzyScore = FuzzyScore(Locale.getDefault())
-    private val logger = LoggerFactory.getLogger(this::class.java)
     private var initFinished: Boolean = false
 
     init {
         val initStart = System.currentTimeMillis()
         val jobList: MutableList<Job> = mutableListOf()
-        val audioDir = File(PropertiesUtil.get(PropertiesUtil.LOCAL_AUDIO_DIR))
+
         if (audioDir.isDirectory && audioDir.canRead()) {
             ioScope.launch {
+                // Split initialization between subdirectories of the root audio directory
                 audioDir.listFiles().forEach {
                     jobList.add(ioScope.launch {
                         if (it.isDirectory) {
@@ -46,6 +48,7 @@ class LocalTrackChecker(
                     })
                 }
                 jobList.joinAll()
+
                 val initDuration = System.currentTimeMillis() - initStart
                 initFinished = true
                 logger.info("Init finished in ${initDuration}ms")
@@ -54,7 +57,7 @@ class LocalTrackChecker(
     }
 
     private fun parseFile(file: File) {
-        if (!file.isDirectory && file.exists() && file.isFile && file.canRead()) {
+        if (file.exists() && file.isFile && file.canRead()) {
             try {
                 val lastDotIndex: Int = file.name.lastIndexOf('.')
                 val fileExtension: String? = if (lastDotIndex >= 0) file.name.substring(lastDotIndex + 1) else null
@@ -66,7 +69,7 @@ class LocalTrackChecker(
                     MediaContainerHints.from(null, fileExtension)
                 ).detectContainer()
 
-                if (detectionResult.isContainerDetected && detectionResult.isSupportedFile) {
+                if (detectionResult.isSupportedFile) {
                     localTracks.add(LocalAudioTrackInfo(detectionResult.trackInfo, file.absolutePath))
                 }
             } catch (e: Exception) {
@@ -80,10 +83,9 @@ class LocalTrackChecker(
         if (!initFinished || identifier.length < MIN_TOTAL_LENGTH_TO_CONSIDER) return null
 
         val searchStartTime = System.currentTimeMillis()
-
         val splitIdentifier = identifier.split("\\s+".toRegex())
 
-        var max: Int = 0
+        var max = 0
         var maxIndex: Int = -1
         localTracks.forEachIndexed { i, localAudioTrackInfo ->
             val trackScore = splitIdentifier.sumOf { piece ->
@@ -95,9 +97,11 @@ class LocalTrackChecker(
                 maxIndex = i
             }
         }
-        val result = if (maxIndex != -1 && max >= identifier.length * FUZZY_THRESHOLD_FACTOR)
+
+        val result = if (maxIndex != -1 && max >= identifier.length * FUZZY_THRESHOLD_FACTOR) {
             localTracks[maxIndex].filePath
-        else null
+        } else null
+
         logger.info("Search finished, took ${System.currentTimeMillis() - searchStartTime}ms. Found result: $result")
         return result
     }
