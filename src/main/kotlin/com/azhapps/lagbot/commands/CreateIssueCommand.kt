7aaa68f5b6f6268ade5df11112301a782bed0fd9
Karl Beier <karl.beier1@gmail.com>
2025-03-24 11:26:12 +1100

    #133 Fix argument processing when creating issues

--- src/main/kotlin/com/azhapps/lagbot/commands/CreateIssueCommand.kt
+++ src/main/kotlin/com/azhapps/lagbot/commands/CreateIssueCommand.kt
@@ -4,10 +4,9 @@ import com.azhapps.lagbot.commands.model.CommandContext
 import org.javacord.api.event.message.MessageCreateEvent
 
 fun Commands.createIssue(context: CommandContext, userInfo: String) {
-    val decomposed = context.arguments.split(' ')
 
     githubRepository.createIssue(
-        title = decomposed.slice(IntRange(1, decomposed.size - 1)).joinToString(" "),
+        title = context.arguments,
         body = "Created by lagbot. Requesting user: $userInfo"
     ) {
         context.onResponse(it)
